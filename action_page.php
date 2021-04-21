5b6763f01c2be92b42908d59e88301a492c3efea
Mary Vogler <mvogler@umich.edu>
2021-04-21 14:03:39 -0400

    contact page

--- action_page.php
+++ action_page.php
@@ -9,3 +9,11 @@ $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Thank You!";
 ?>
+
+<?php
+$conn = mysqli_connect("localhost", "root", "test", "blog_samples") or die("Connection Error: " . mysqli_error($conn));
+mysqli_query($conn, "INSERT INTO tblcontact (firstname, lastname, email, message) VALUES ('" . $firstname. "', '" . $lastname. "','" . $email. "','" . $message. "')");
+$insert_id = mysqli_insert_id($conn);
+if(!empty($insert_id)) {
+$message = "Your contact information is saved successfully";
+?>
